587216146de36789d2088c5de3a5414fba260fac
diajengtriana <diajengtriana11@gmail.com>
2021-11-03 16:50:33 +0700

    upload

---
+++ hasilpenghitungan.py
@@ -0,0 +1,11 @@
+print ("Hallo")
+print ("Program menghitung luas dan keliling lingkaran")
+
+phi=3.14
+r=float(input("\nMasukkan jari-jari:"))
+
+luas=phi*r*r
+keliling=2*phi*r
+
+print("Luas lingkaran=" +str(luas))
+print("Keliling lingkaran=" +str(keliling))
