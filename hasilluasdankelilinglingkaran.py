88b7224a5dffd5b44fdc1c7aca7fa2efb672f29e
diajengtriana <diajengtriana11@gmail.com>
2021-11-04 13:52:01 +0700

    upload

---
+++ hasilluasdankelilinglingkaran.py
@@ -0,0 +1,15 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+Windows PowerShell
+Copyright (C) Microsoft Corporation. All rights reserved.
+
+Try the new cross-platform PowerShell https://aka.ms/pscore6
+
+PS D:\Luas bangunan> & C:/Users/Diajeng/AppData/Local/Programs/Python/Python310/python.exe "d:/Luas bangunan/Luasdankelilinglingkaran/hasilpenghitungan.py"
+Hallo
+Program menghitung luas dan keliling lingkaran
+
+Masukkan jari-jari:28
+Luas lingkaran=2461.76   
+Keliling lingkaran=175.84
+PS D:\Luas bangunan> 
